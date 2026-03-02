2375bb9b3b0a029df8748877f4653bc3608d2640
Rahul Gupta <rahulgupta2072485@gmail.com>
2026-03-02 18:43:39 +0530

    Revert "AutoVerify feature turned on"
    This reverts commit 6ed326b885e4c4c1ee48963f2645d029b545fadd.

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -38,17 +38,11 @@ const register = async (req, res, next) => {
     const passwordHash = await User.hashPassword(password);
 
     // Create user
-    console.log('[REGISTER] Creating user:', email);
-    const newUser = await User.create({ email, password: passwordHash, name });
-    console.log('[REGISTER] User created with ID:', newUser.id);
+    console.log(`[REGISTER] Creating user: ${email}`);
+    const newUser = await User.createUser(email, passwordHash, name);
+    console.log(`[REGISTER] User created with ID: ${newUser.id}`);
 
-    // Auto-verify the user's email to bypass email sending issues for now
-    console.log('[REGISTER] Auto-verifying user email for ID:', newUser.id);
-    await User.verifyUserEmail(newUser.id);
-    console.log('[REGISTER] User email auto-verified.');
-
-    // Generate verification token and send email in the background
-    // This part will still run but won't block login
+    // Generate verification token
     console.log('[REGISTER] Generating verification token');
     const { token, expiresAt } = generateVerificationToken();
     
